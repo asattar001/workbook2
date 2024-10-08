d7500cb60b785fac26b79b54b5d9f2b9171c318b
Abdulkadir Sattar <abdulkadirsttr@gmail.com>
2024-10-08 10:46:10 -0400

    js example

---
+++ hello.js
@@ -0,0 +1,8 @@
+// Example 1: declare and assign same line
+var message = "Hello";
+console.log(message);
+
+// Example 2: declare and assign seperately
+var message2;
+message2 = "Hello";
+console.log(message2);
